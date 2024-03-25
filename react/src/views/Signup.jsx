a903c07e14fc350e7e4a7aa2cf0082476139e985
Furkan Sünger <furkan.sunger@outlook.com>
2024-03-25 08:08:08 +0300

    Signup done in React

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,7 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom';
-import axiosClient from '../axios-client';
-import { useStateContext } from '../contexts/ContextProvider';
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Signup = () => {
   const nameRef = useRef();
@@ -9,47 +9,83 @@ const Signup = () => {
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
 
+  const [errors, setErrors] = useState(null);
+
   const { setToken, setUser } = useStateContext();
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const payload = {
       name: nameRef.current.value,
       email: emailRef.current.value,
       password: passwordRef.current.value,
-      password_confirmation: passwordConfirmationRef.current.value
+      password_confirmation: passwordConfirmationRef.current.value,
     };
 
-    axiosClient.post('/signup', payload)
+    axiosClient
+      .post("/signup", payload)
       .then(({ data }) => {
         setUser(data.user);
         setToken(data.token);
       })
-      .catch(error => {
+      .catch((error) => {
         const response = error.response;
 
         if (response && response.status === 422) {
-          console.log(response.data.errors);
+          setErrors(response.data.errors);
         }
-      })
-  }
+      });
+  };
 
   return (
-    <div className='login-signup-form animated fadeInDown'>
+    <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Signup for free</h1>
-          <input ref={nameRef} type="text" name="" id="" placeholder='Full Name' />
-          <input ref={emailRef} type="email" name="" id="" placeholder='Email' />
-          <input ref={passwordRef} type="password" name="" id="" placeholder='Password' />
-          <input ref={passwordConfirmationRef} type="password" name="" id="" placeholder='Password Confirmation' />
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )}
+          <input
+            ref={nameRef}
+            type="text"
+            name=""
+            id=""
+            placeholder="Full Name"
+          />
+          <input
+            ref={emailRef}
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+          />
+          <input
+            ref={passwordRef}
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+          />
+          <input
+            ref={passwordConfirmationRef}
+            type="password"
+            name=""
+            id=""
+            placeholder="Password Confirmation"
+          />
           <button className="btn btn-block">Signup</button>
-          <p className="message">Already Registered? <Link to='/login'>Sign in</Link></p>
+          <p className="message">
+            Already Registered? <Link to="/login">Sign in</Link>
+          </p>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
