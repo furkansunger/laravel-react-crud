f47a5c147559c4ef2d64db95575cc02cb1122028
Furkan Sünger <furkan.sunger@outlook.com>
2024-03-26 07:34:22 +0300

    Logout done

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios-client";
 
 const DefaultLayout = () => {
-    const { user, token } = useStateContext();
+    const { user, token, setToken, setUser } = useStateContext();
 
     if (!token) {
         return <Navigate to="/login" />;
     }
 
-    const onLogout = e => {
-        e.prefentDefault();
-    }
+    const onLogout = (e) => {
+        e.preventDefault();
+
+        axiosClient.post("/logout").then(() => {
+            setUser({});
+            setToken(null);
+        });
+    };
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({ data }) => {
+            setUser({ data });
+        });
+    }, []);
 
     return (
         <div id="defaultLayout">
             <aside>
-                <Link to='/dashboard'>Dashboard</Link>
-                <Link to='/users'>Users</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
             </aside>
             <div className="content">
                 <header>
+                    <div>Header</div>
                     <div>
-                        Header
-                    </div>
-                    <div>
-                        {user.name}
-                        <a className="btn-logout" href="#" onClick={onLogout}>Logout</a>
+                        {user?.data?.name}
+                        <a className="btn-logout" href="#" onClick={onLogout}>
+                            Logout
+                        </a>
                     </div>
                 </header>
 
