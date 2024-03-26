a5a1d6f65966921383a78b0af5c8d02d1bf02145
Furkan Sünger <furkan.sunger@outlook.com>
2024-03-26 06:58:01 +0300

    Login done

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
         if (!Auth::attempt($credentials)) {
             return response([
                 'message' => 'Provided email address or password is incorrect',
-            ]);
+            ], 422);
         }
 
         /** @var User $user */
--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,25 +1,82 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-  const onSubmit = e => {
+  const [errors, setErrors] = useState(null);
+
+  const { setToken, setUser } = useStateContext();
+
+  const onSubmit = (e) => {
     e.preventDefault();
-  }
+
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    setErrors(null);
+
+    axiosClient
+      .post("/login", payload)
+      .then(({ data }) => {
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch((error) => {
+        const response = error.response;
+
+        if (response && response.status === 422) {
+          if (response.data.errors) {
+            setErrors(response.data.errors);
+          } else {
+            setErrors({
+              email: [response.data.message]
+            });
+          }
+        }
+      });
+  };
 
   return (
-    <div className='login-signup-form animated fadeInDown'>
+    <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Login into your account</h1>
-          <input type="email" name="" id="" placeholder='Email' />
-          <input type="password" name="" id="" placeholder='Password' />
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )}
+          <input
+            ref={emailRef}
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+          />
+          <input
+            ref={passwordRef}
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+          />
           <button className="btn btn-block">Login</button>
-          <p className="message">Not Registered? <Link to='/signup'>Create an account</Link></p>
+          <p className="message">
+            Not Registered?{" "}
+            <Link to="/signup">Create an account</Link>
+          </p>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
